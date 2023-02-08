16bf3e14607fed24b6b0fb2a2df6a12b0fc0a718
Naufal <67991338+SiNopal1@users.noreply.github.com>
2023-02-08 16:31:08 +0700

    .

---
+++ menu/databasemealplan.js
@@ -0,0 +1,59 @@
+karbohidrat = [
+    { "makanan": "Nasi putih", "kalori": 130, "Berat": "100 gram" },
+    { "makanan": "Nasi Merah", "kalori": 110, "Berat": "100 gram" },
+    { "makanan": "Singkong", "kalori": 160, "Berat": "100 gram" },
+    { "makanan": "Kentang", "kalori": 77, "Berat": "100 gram" },
+    { "makanan": "Oatmeal", "kalori": 68, "Berat": "100 gram" },
+    { "makanan": "Jagung", "kalori": 86, "Berat": "100 gram" },
+    { "makanan": "Ubi jalar", "kalori": 86, "Berat": "100 gram" }
+]
+
+protein = [
+    { "makanan": "Telur", "kalori": 155, "Berat": "100 gram" },
+    { "makanan": "Dada Ayam", "kalori": 165, "Berat": "100 gram" },
+    { "makanan": "Daging sapi", "kalori": 251, "Berat": "100 gram " },
+    { "makanan": "Salmon", "kalori": 209, "Berat": "100 gram" },
+    { "makanan": "Udang", "kalori": 100, "Berat": "100 gram" },
+    { "makanan": "Ikan Tuna", "kalori": 132, "Berat": "100 gram" },
+    { "makanan": "Susu", "kalori": 43, "Berat": "100 gram" },
+    { "makanan": "Tempe", "kalori": 193, "Berat": "100 gram" }
+]
+
+lemak = [
+    { "makanan": "Ikan salmon", "kalori": 208, "Berat": "100 gram" },
+    { "makanan": "Dark chocolate", "kalori": 180, "Berat": "30 gram" },
+    { "makanan": "Alpukat", "kalori": 160, "Berat": "100 gram" },
+    { "makanan": "Kacang almond", "kalori": 575, "Berat": "100 gram" },
+    { "makanan": "Tahu", "kalori": 76, "Berat": "100 gram" },
+    { "makanan": "Susu kedelai", "kalori": 54, "Berat": "100 gram" },
+    { "makanan": "Edamame", "kalori": 121, "Berat": "100 gram" },
+    { "makanan": "Keju", "kalori": 100, "Berat": "25 gram" }
+]
+
+buah = [
+    { "makanan": "Apel", "kalori": 52, "Berat": "100 gram" },
+    { "makanan": "Nanas", "kalori": 50, "Berat": "100 gram" },
+    { "makanan": "Mangga", "kalori": 60, "Berat": "100 gram" },
+    { "makanan": "Jambu Biji", "kalori": 68, "Berat": "100 gram" },
+    { "makanan": "Delima", "kalori": 83, "Berat": "100 gram" },
+    { "makanan": "Pisang", "kalori": 89, "Berat": "100 gram" },
+    { "makanan": "Semangka", "kalori": 30, "Berat": "100 gram" },
+    { "makanan": "Buah Naga", "kalori": 51, "Berat": "100 gram" },
+    { "makanan": "Alpukat", "kalori": 160, "Berat": "100 gram" },
+    { "makanan": "Stroberi", "kalori": 32, "Berat": "100 gram" }
+]
+
+sayur = [
+    { "makanan": "Sayur bayam", "kalori": 23, "Berat": "100 gram" },
+    { "makanan": "Brokoli", "kalori": 34, "Berat": "100 gram" },
+    { "makanan": "Daun singkong", "kalori": 38, "Berat": "100 gram" },
+    { "makanan": "Kangkung", "kalori": 19, "Berat": "100 gram" },
+    { "makanan": "Kacang Panjang", "kalori": 47, "Berat": "100 gram" },
+    { "makanan": "Tauge", "kalori": 23, "Berat": "100 gram" },
+    { "makanan": "Daun Bawang", "kalori": 32, "Berat": "100 gram" },
+    { "makanan": "Sawi", "kalori": 12, "Berat": "100 gram" },
+    { "makanan": "Seledri", "kalori": 14, "Berat": "100 gram" },
+    { "makanan": "Buncis", "kalori": 31, "Berat": "100 gram" },
+    { "makanan": "Selada", "kalori": 15, "Berat": "100 gram" },
+    { "makanan": "Kemanggi", "kalori": 23, "Berat": "100 gram" }
+]
